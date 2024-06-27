7da0bda8d08d82198d832f750f844a9e1270307f
Ezy <ezyfaturohman@gmail.com>
2024-06-27 21:14:32 +0700

    add middleware not done

--- middleware/analytics.global.ts
+++ middleware/analytics.global.ts
@@ -2,17 +2,24 @@ import { useAuth } from "~/stores/auth";
 import Cookies from 'js-cookie';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-
     const auth = useAuth();
+
+    // Panggil checkAuth dan tunggu sampai selesai
     await auth.checkAuth();
-    const token = Cookies.get('jwt');
 
+    // Log untuk debugging
+    console.log('auth.userLogin:', auth.userLogin);
+    console.log('Navigating to:', to.path);
+
+    // Jika pengguna sudah login dan mencoba mengakses halaman login atau register
     if (auth.userLogin && (to.path === '/login' || to.path === '/register')) {
-        return navigateTo("/")
+        console.log('User is logged in, redirecting to home');
+        return navigateTo('/', { replace: true });
     }
 
+    // Jika pengguna belum login dan mencoba mengakses halaman yang membutuhkan login
     if (!auth.userLogin && to.path.startsWith('/user')) {
-        return navigateTo('/login')
+        console.log('User is not logged in, redirecting to login');
+        return navigateTo('/login', { replace: true });
     }
-
-}); 
+});
