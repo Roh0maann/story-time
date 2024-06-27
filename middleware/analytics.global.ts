36ef83ae2adc85d6030daa35c6ceb4e026ae49fe
Ezy <ezyfaturohman@gmail.com>
2024-06-27 20:53:21 +0700

    fix error fetch

--- middleware/analytics.global.ts
+++ middleware/analytics.global.ts
@@ -3,16 +3,16 @@ import Cookies from 'js-cookie';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
 
-    // const auth = useAuth();
-    // await auth.checkAuth();
-    // const token = Cookies.get('jwt');
+    const auth = useAuth();
+    await auth.checkAuth();
+    const token = Cookies.get('jwt');
 
-    // if (auth.userLogin && (to.path === '/login' || to.path === '/register')) {
-    //     return navigateTo("/")
-    // }
+    if (auth.userLogin && (to.path === '/login' || to.path === '/register')) {
+        return navigateTo("/")
+    }
 
-    // if (!auth.userLogin && to.path.startsWith('/user')) {
-    //     return navigateTo('/login')
-    // }
+    if (!auth.userLogin && to.path.startsWith('/user')) {
+        return navigateTo('/login')
+    }
 
 }); 
