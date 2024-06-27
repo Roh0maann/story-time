64da7a22bbc2e7c621a941bed7e120c339f01fc0
Ezy <ezyfaturohman@gmail.com>
2024-06-27 21:51:34 +0700

    error server

--- middleware/analytics.global.ts
+++ middleware/analytics.global.ts
@@ -7,19 +7,19 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     // Panggil checkAuth dan tunggu sampai selesai
     await auth.checkAuth();
 
-    // Log untuk debugging
-    console.log('auth.userLogin:', auth.userLogin);
-    console.log('Navigating to:', to.path);
+    // // Log untuk debugging
+    // console.log('auth.userLogin:', auth.userLogin);
+    // console.log('Navigating to:', to.path);
 
-    // Jika pengguna sudah login dan mencoba mengakses halaman login atau register
-    if (auth.userLogin && (to.path === '/login' || to.path === '/register')) {
-        console.log('User is logged in, redirecting to home');
-        return navigateTo('/', { replace: true });
-    }
+    // // Jika pengguna sudah login dan mencoba mengakses halaman login atau register
+    // if (auth.userLogin && (to.path === '/login' || to.path === '/register')) {
+    //     console.log('User is logged in, redirecting to home');
+    //     return navigateTo('/', { replace: true });
+    // }
 
-    // Jika pengguna belum login dan mencoba mengakses halaman yang membutuhkan login
-    if (!auth.userLogin && to.path.startsWith('/user')) {
-        console.log('User is not logged in, redirecting to login');
-        return navigateTo('/login', { replace: true });
-    }
+    // // Jika pengguna belum login dan mencoba mengakses halaman yang membutuhkan login
+    // if (!auth.userLogin && to.path.startsWith('/user')) {
+    //     console.log('User is not logged in, redirecting to login');
+    //     return navigateTo('/login', { replace: true });
+    // }
 });
